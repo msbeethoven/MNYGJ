abd07348b99f61de41ce30d53dc36d194f66f802
claudegrant <grant.claude@gmail.com>
2019-04-13 22:12:43 -0400

    W.I.P.
    Added scene change scripts & scripts for enabling/disabling objects over time...

---
+++ MNYGJ/Assets/Katsassets/Scripts/ButtonHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement; 
+
+public class ButtonHandler : MonoBehaviour
+{
+public void playBtn(){
+    SceneManager.LoadScene("AvatarSelection"); 
+}
+
+public void leaveBtn(){
+    Application.Quit(); 
+    Debug.Log("Quit Application");
+}
+
+public void loadCutScene() {
+
+    SceneManager.LoadScene("IntroCutScene"); 
+}
+
+public void loadAvatarSelection(){
+    
+    SceneManager.LoadScene("AvatarSelection"); 
+}
+
+public void loadMiniMap() {
+
+    SceneManager.LoadScene("MiniMap"); 
+
+}
+
+public void loadRunner() {
+
+    SceneManager.LoadScene("Runner"); 
+}
+}
