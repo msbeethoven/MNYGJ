060f0a25489dc9b92bf2413534c0f5aa2c2dc923
core <seorisachs@gmail.com>
2019-04-13 20:22:09 -0400

    Movement, starting touch direction

--- MNYGJ/Assets/Scripts/PlayerJump.cs
+++ MNYGJ/Assets/Scripts/PlayerJump.cs
@@ -5,38 +5,25 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour
 {
   public float moveSpeed = 3f;
-
   public float counter = 0;
   public float moveForce = 10f;
   private bool isMoving = false;
-
-  private bool shouldLerp = false;
-
-  public float timeStartedLerping;
-  public float lerpTime;
   public Vector2 endPosition;
   public Vector2 startPosition;
-  private Rigidbody2D rigidbody;
+  private new Rigidbody2D rigidbody;
 
-
-  private void StartLerping()
-  {
-    timeStartedLerping = Time.time;
-    shouldLerp = true;
-
-  }
   // Start is called before the first frame update
 
   void Start()
   {
     rigidbody = GetComponent<Rigidbody2D>();
 
-
   }
 
   // Update is called once per frame
   void Update()
   {
+
     if (!isMoving)
     {
 
@@ -44,13 +31,12 @@ public class PlayerJump : MonoBehaviour
       {
         StartCoroutine("MoveForward");
         counter++;
-        
+
         if (counter % 2 != 0)
         {
-          Debug.Log("ya");
           StopCoroutine("MoveForward");
           isMoving = false;
-          
+
         }
 
       }
@@ -58,25 +44,14 @@ public class PlayerJump : MonoBehaviour
     }
   }
 
-  public Vector3 Lerp(Vector3 start, Vector3 end, float timeStartedLerping, float lerpTime = 1)
-  {
-
-    float timeSinceStarted = Time.time - timeStartedLerping;
-
-    float percentageComplete = timeSinceStarted / lerpTime;
-
-    var result = Vector3.Lerp(start, end, percentageComplete);
-    return result;
-
-    transform.position = Lerp(startPosition, endPosition, timeStartedLerping, lerpTime);
-
-  }
 
   private IEnumerator MoveForward()
   {
     isMoving = true;
-    //rigidbody.AddForce(new Vector2(10f, moveForce));
-    transform.Translate(Vector3.up * 10 * Time.deltaTime);
+    // Touch touch = Input.GetTouch(1);
+
+    // Debug.Log(touch.position);
+    transform.Translate(Vector3.up * 20 * Time.deltaTime);
 
     yield return new WaitForSeconds(.1f);
     isMoving = false;
