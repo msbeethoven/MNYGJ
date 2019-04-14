86078b797981489b570e232378e9153b7b097522
claudegrant <grant.claude@gmail.com>
2019-04-14 15:20:39 -0400

    Added Buttons & Cleaned Assets

--- MNYGJ/Assets/katsScenes/Scripts/toggleObject.cs
+++ MNYGJ/Assets/katsScenes/Scripts/toggleObject.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class toggleObject : MonoBehaviour
 {
-   public float delay = 3f; 
+   public float delay = 1f; 
+   public float delay2 =2f; 
+
+   public float delay3 =4f; 
+   public float delay4 = 5f; 
    public GameObject CharacterDialogue;
    public GameObject CustomizeAvatar; 
 
@@ -24,8 +28,13 @@ public class toggleObject : MonoBehaviour
     CustomizeAvatar.SetActive(false); 
     CharacterDialogue.SetActive(true); 
     CharacterDialogue.transform.GetChild(2).gameObject.SetActive(false);
-    
+    CharacterDialogue.transform.GetChild(3).gameObject.SetActive(false); 
+    CharacterDialogue.transform.GetChild(4).gameObject.SetActive(false); 
+    CharacterDialogue.transform.GetChild(5).gameObject.SetActive(false); 
+
     StartCoroutine("loadObjects", delay); 
+    StartCoroutine("loadObjects", delay2);
+    StartCoroutine("loadObjects", delay3); 
     }
     IEnumerator loadObjects (float delay)
     {
@@ -33,8 +42,18 @@ public class toggleObject : MonoBehaviour
       yield return new WaitForSeconds(delay);
       Debug.Log("succesfully got past the wait for seconds");
       CharacterDialogue.transform.GetChild(2).gameObject.SetActive(true);
-     
+      yield return new WaitForSeconds(delay2); 
+      CharacterDialogue.transform.GetChild(2).gameObject.SetActive(false); 
+      CharacterDialogue.transform.GetChild(3).gameObject.SetActive(true); 
+      yield return new WaitForSeconds(delay3); 
+      CharacterDialogue.transform.GetChild(3).gameObject.SetActive(false);
+      CharacterDialogue.transform.GetChild(4).gameObject.SetActive(true); 
+      CharacterDialogue.transform.GetChild(5).gameObject.SetActive(true); 
+
     }
+  
+
+    
 }
 
 
