34ea86fec0d1517f43028314594c92da7aa04437
core <seorisachs@gmail.com>
2019-04-20 22:06:21 -0400

    Correct avatar appears

---
+++ MNYGJ/Assets/katsScenes/Scripts/DisplayCurrentAvatar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayCurrentAvatar : MonoBehaviour
+{
+  // Start is called before the first frame update
+  public int currentAvatarIndex;
+
+  public Image _avatarImage;
+  public AvatarSprites _sprites;
+  void Start()
+  {
+    currentAvatarIndex = PlayerPrefs.GetInt("AvatarIndex");
+    DisplayAvatar();
+
+  }
+
+  public void DisplayAvatar()
+  {
+    _avatarImage.sprite = _sprites.Avatars[currentAvatarIndex];
+  }
+
+
+}
